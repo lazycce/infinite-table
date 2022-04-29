b0d84de636858adcd281881020aa4e6b63d018ec
luhaotian <haotian.lu@proaimitd.com>
2022-04-29 11:18:47 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'infinite-table.min.js',
+  },
+  devtool: "source-map",
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })],
+}
